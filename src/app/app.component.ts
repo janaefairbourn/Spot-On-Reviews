b67b51e9b90c471e145c3db24ee4321c7467f6f9
janaefairbourn <fairbourn.janae@gmail.com>
2019-03-30 14:30:25 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mis-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'spot-on-reviews';
+}
